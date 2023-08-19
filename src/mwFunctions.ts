f9eae96deb75f789bdc41366b33caf0010ccc50d
kaz <kazuki.amatatsu.fd@gmail.com>
2023-08-19 19:13:03 +0900

    Bパターン - ログインしている状態でログイン後に見れるページがチラつくかどうか
    動作確認結果
    admin権限でログインしている状態で検証、ログイン後に見れるページがチラつかない

--- src/mwFunctions.ts
+++ src/mwFunctions.ts
@@ -23,10 +23,10 @@ export const isHogePage = (request: NextRequest) => {
 
 /** login済みかのチェックを行う */
 export const isAuthorized = (request: NextRequest, appEnv: string) => {
-  return false;
-  // if (appEnv === 'local') {
-  //   return true;
-  // }
+  // return false;
+  if (appEnv === 'local') {
+    return true;
+  }
   // NOTE: access_tokenはSecureとHttpOnlyによって悪意のあるサイトからのCookieの盗み出しを防いでいる
   // 万が一があっても、REST APIを叩く時にはサーバーサイド側でエラーを返すため、ログイン後にしか見れない情報が漏洩することはない
   return request.cookies.has('access_token');

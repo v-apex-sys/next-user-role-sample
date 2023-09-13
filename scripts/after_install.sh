dcf1cc97e6c8ca6b1079c3ccc9b25d0df54aa6b6
kichishima <kichishima@v-apex.co.jp>
2023-09-13 12:59:04 +0900

    package.jsonに関するデプロイの記述を修正５

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,4 +6,4 @@
 #####################################
 echo 'before install'
 cd /var/www/html/rentals
-echo { \"type\":\"module\" } >> ./package.json # デプロイサーバに設置するpackage.jsonを生成
+echo { \"type\":\"module\" } > ./package.json # デプロイサーバに設置するpackage.jsonを生成（or上書き）
